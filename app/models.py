5cd53681f0f2501ef7ee0b3b7f667cad80a8d6ab
Gilles <gilloubourdin@gmail.com>
2026-05-19 15:47:20 +0200

    Add User model

--- app/models.py
+++ app/models.py
@@ -5,16 +5,19 @@ from sqlalchemy.orm import relationship
 
 from app.database import Base
 
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
 
 class Cave(Base):
     __tablename__ = "caves"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
-    email = Column(String, unique=True, nullable=False, index=True)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
     name = Column(String, nullable=False)
 
     region = Column(String, default="Vaud")
